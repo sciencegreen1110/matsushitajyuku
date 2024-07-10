b60352dd0f689c108a5ba12a5ee61a5ad3895887
MathshitaKazu <sciencegreen1110@gmail.com>
2024-07-10 16:17:07 +0900

    matsushitajyukuSample
    れんしゅうです。

---
+++ index_sec02.php
@@ -0,0 +1,144 @@
+<section id="sec02" class="sec02">
+  <div class="inner fadeIn">
+    <div class="sec02__msgContainer">
+      <h1 class="sec02__h1">
+        個別指導のいらない<br>
+        初学者でも学びやすい仕組み
+      </h1>
+      <ul class="sec02__ul">
+
+        <li class="sec02__step fadeLeft">
+          <div class="sec02__step__number">１</div>
+          <div class="sec02__step__container">
+            <div class="sec02__step__img-box">
+              <img src="images/douga-contents.jpg" alt="オンライン動画学習" class="sec02__step__img-box__img">
+            </div>
+            <div class="sec02__step__msg-box">
+              <h3 class="sec02__step__msg-box__h3">
+                「１回５分〜１０分」のオンライン動画学習
+              </h3>
+              <p class="sec02__step__msg-box__p">
+                学習は全て動画での自習型。１レッスンの動画は集中力が保たれる５分〜１０分。<br>
+                通学時間などのスキマ時間でも学習ができます。
+              </p>
+            </div>
+          </div>
+        </li>
+
+        <li class="sec02__step fadeLeft">
+          <div class="sec02__step__number">２</div>
+          <div class="sec02__step__container">
+            <div class="sec02__step__img-box">
+              <img src="images/chat.jpg" alt="チャットで質問" class="sec02__step__img-box__img">
+            </div>
+            <div class="sec02__step__msg-box">
+              <h3 class="sec02__step__msg-box__h3">
+                塾長：松下にチャットで質問
+              </h3>
+              <p class="sec02__step__msg-box__p">
+                分からないところは、塾長自らがチャットで質問に答えます！<br>
+                すぐに答えを教えるのではなく、あくまで思考力を鍛えられるように誘導します。
+              </p>
+            </div>
+          </div>
+        </li>
+
+        <li class="sec02__step fadeLeft">
+          <div class="sec02__step__number">３</div>
+          <div class="sec02__step__container">
+            <div class="sec02__step__img-box">
+              <img src="images/test.jpg" alt="単元別定着度テスト" class="sec02__step__img-box__img">
+            </div>
+            <div class="sec02__step__msg-box">
+              <h3 class="sec02__step__msg-box__h3">
+                単元別定着度テストで習熟度徹底確認
+              </h3>
+              <p class="sec02__step__msg-box__p">
+                毎回の単元で、定着度テストに合格しないと次の単元に進むことができない仕組み！<br>
+                合格できるまで、徹底サポート！
+              </p>
+            </div>
+          </div>
+        </li>
+
+        <li class="sec02__step fadeLeft">
+          <div class="sec02__step__number">４</div>
+          <div class="sec02__step__container">
+            <div class="sec02__step__img-box">
+              <img src="images/shingakushiryou.jpg" alt="進路相談サポート" class="sec02__step__img-box__img">
+            </div>
+            <div class="sec02__step__msg-box">
+              <h3 class="sec02__step__msg-box__h3">
+                データに基づく適格な進学指導力
+              </h3>
+              <p class="sec02__step__msg-box__p">
+                ２万人を超える卒業生の進学データをもとに進学資料冊子を作成し、<br>現役の受験生の進路相談に応じます！
+              </p>
+            </div>
+          </div>
+        </li>
+
+        <!-- ここからスマホ版１-->
+        <li class="sec02__step--sm fadeLeft">
+          <div class="sec02__step--sm__number">１</div>
+          <div class="sec02__step--sm__img-box">
+            <img src="images/douga-contents.jpg" alt="オンライン動画学習" class="sec02__step--sm__img-box__img">
+          </div>
+          <h3 class="sec02__step--sm__msg-box__h3">
+            「１回５分〜１０分」のオンライン動画学習
+          </h3>
+          <p class="sec02__step--sm__msg-box__p">
+            学習は全て動画での自習型。１レッスンの動画は集中力が保たれる５分〜１０分。<br>
+            通学時間などのスキマ時間でも学習ができます。
+          </p>
+        </li>
+        <!-- ここまでスマホ版１ -->
+        <!-- ここからスマホ版２-->
+        <li class="sec02__step--sm fadeLeft">
+          <div class="sec02__step--sm__number">２</div>
+          <div class="sec02__step--sm__img-box">
+            <img src="images/chat.jpg" alt="チャットで質問" class="sec02__step--sm__img-box__img">
+          </div>
+          <h3 class="sec02__step--sm__msg-box__h3">
+            塾長：松下にチャットで質問
+          </h3>
+          <p class="sec02__step--sm__msg-box__p">
+            分からないところは、塾長自らがチャットで質問に答えます！<br>
+            すぐに答えを教えるのではなく、あくまで思考力を鍛えられるように誘導します。
+          </p>
+        </li>
+        <!-- ここまでスマホ版２ -->
+        <!-- ここからスマホ版３-->
+        <li class="sec02__step--sm fadeLeft">
+          <div class="sec02__step--sm__number">３</div>
+          <div class="sec02__step--sm__img-box">
+            <img src="images/test.jpg" alt="単元別定着度テスト" class="sec02__step--sm__img-box__img">
+          </div>
+          <h3 class="sec02__step--sm__msg-box__h3">
+            単元別定着度テストで習熟度徹底確認
+          </h3>
+          <p class="sec02__step--sm__msg-box__p">
+            毎回の単元で、定着度テストに合格しないと次の単元に進むことができない仕組み！<br>
+            合格できるまで、徹底サポート！
+          </p>
+        </li>
+        <!-- ここまでスマホ版３ -->
+        <!-- ここからスマホ版４-->
+        <li class="sec02__step--sm fadeLeft">
+          <div class="sec02__step--sm__number">４</div>
+          <div class="sec02__step--sm__img-box">
+            <img src="images/shingakushiryou.jpg" alt="進路相談サポート" class="sec02__step--sm__img-box__img">
+          </div>
+          <h3 class="sec02__step--sm__msg-box__h3">
+            データに基づく適格な進学指導力
+          </h3>
+          <p class="sec02__step--sm__msg-box__p">
+            ２万人を超える卒業生の進学データをもとに進学資料冊子を作成し、<br>現役の受験生の進路相談に応じます！
+          </p>
+        </li>
+        <!-- ここまでスマホ版３ -->
+
+
+
+
+</section>
